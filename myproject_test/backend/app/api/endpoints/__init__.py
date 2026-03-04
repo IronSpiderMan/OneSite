ba2d91af8330bc0d404d0982e4df39e5c796f6fa
ZackFair <sockwz@gmail.com>
2026-03-04 15:35:50 +0800

    删除测试项目

--- myproject_test/backend/app/api/endpoints/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-# @Author: ZackFair
-# @Desc: 
-# @File: __init__.py
-# @Date: 2026/2/10 09:06
--- myproject_test/backend/app/schemas/token.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from sqlmodel import SQLModel
-
-class Token(SQLModel):
-    access_token: str
-    token_type: str
-
-class TokenPayload(SQLModel):
-    sub: Optional[int] = None
